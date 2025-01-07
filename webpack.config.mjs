6a8676ba8249319a152d837293f4cfeaad84c758
Lars Bodewig <40965749+LarsBodewig@users.noreply.github.com>
2025-01-07 01:05:52 +0100

    Index and background

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,31 +1,41 @@
 import { resolve as _resolve } from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
 import sqlJs from "sql.js";
 import fs from "fs";
 
+function query(db, stmt, params) {
+  const { columns, values } = db.exec(stmt, params ?? {})[0];
+  return values.map((row) =>
+    Object.fromEntries(
+      row.map((value, index) => {
+        const column = columns[index];
+        return [column, value];
+      })
+    )
+  );
+}
+
 const data = await sqlJs({
   locateFile: (file) => `node_modules/sql.js/dist/${file}`,
-}).then(sql => {
+}).then((sql) => {
   const buffer = fs.readFileSync("./database.sqlite");
   const db = new sql.Database(new Uint8Array(buffer));
-  const { columns, values } = db.exec("select name, url, hintergrund from pilists")[0];
-  const preparedStmt = db.prepare("select musik.kuenstler, musik.titel, musik.url from pilists " 
-    + " left join 'mu-pi' on pilists.name = 'mu-pi'.name " 
-    + " left join musik on ('mu-pi'.kuenstler = musik.kuenstler and 'mu-pi'.titel = musik.titel)"
-    + " where pilists.name = :name");
-  const pilists = values.map(row => {
-    const pilistName = row[0];
-    preparedStmt.bind([":name", pilistName]);
-    const songs = preparedStmt.getAsObject();
+  const playlists = query(db, "select name, url, hintergrund from pilists");
+  const pilists = playlists.map((playlist) => {
+    const songs = query(
+      db,
+      "select musik.kuenstler, musik.titel, musik.url from pilists " +
+        " left join 'mu-pi' mupi on pilists.name = mupi.name " +
+        " left join musik on (mupi.kuenstler = musik.kuenstler and mupi.titel = musik.titel)" +
+        " where pilists.name = :name",
+      { ":name": playlist.name }
+    );
     return {
-      values: Object.fromEntries(row.map((value, index) => {
-          const column = columns[index]
-          return [column, value];
-        })),
-      songs
-    }
+      meta: playlist,
+      songs,
+    };
   });
-  preparedStmt.free();
   db.close();
   return pilists;
 });
@@ -53,16 +63,27 @@ export default {
     extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+      pilists: data.map((pilist) => ({
+        url: pilist.meta.url,
+        name: pilist.meta.name,
+      })),
+    }),
     ...data.map(
-      (pilist) => {
-        console.log(pilist);
-        return new HtmlWebpackPlugin({
-          filename: pilist.values.url + '.html',
-          title: pilist.values.name,
+      (pilist) =>
+        new HtmlWebpackPlugin({
+          filename: pilist.meta.url + "/index.html",
+          title: pilist.meta.name,
           template: "./src/pilist.html",
+          background: "./test.gif", //pilist.meta.hintergrund,
+          songs: pilist.songs,
         })
-      }
     ),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "static" }],
+    }),
   ],
   output: {
     filename: "bundle.js",
